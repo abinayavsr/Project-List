514fd672b9ae2c3b12600b6afb663b3ab6a84351
abinayavsr <143706816+abinayavsr@users.noreply.github.com>
2024-09-07 11:30:40 +0530

    Add files via upload

---
+++ patclas.java
@@ -0,0 +1,32 @@
+package patternpack1;
+import java.util.*;
+public class patclas {
+	public static void main(String args[]) {
+	Scanner sc=new Scanner(System.in);
+	 int no=sc.nextInt();
+	 for(int i=1;i<=no;i++) {
+		 for(int j=1;j<=no;j++){
+			 if(i==1||i==no||j==1||j==no) {
+				 System.out.print("*");
+				 
+			 }
+			 else {
+				 System.out.print(" ");
+			 }}
+			 System.out.println( );
+		 
+			 
+			 
+		
+	 }
+}
+}
+
+
+
+
+
+
+
+
+
